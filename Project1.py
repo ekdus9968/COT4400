e1b94934bf2821a4bd60611f379164ddb4c9be9f
ekdus9968 <kan.seyoung1018@gmail.com>
2026-02-22 20:29:07 -0500

    delete all of description

--- Project1.py
+++ Project1.py
@@ -2,44 +2,6 @@
 Team Member: 
 Seyoung Kan (U36444259)
 Kaushik Selvakumar (U75300364)
-< Project1 Recursive Peoblem Solving >
-This project focuses only on recursion.
-Project 1 must use recursion as the primary solution technique.
-Iteration-based solutions are not allowed for this project.
-
-< Project Idea Description >
-Given a maze represented as a grid (0 = open path / 1 = wall/blocked path)
-
-Goal :
-Find one vaild path from start poisition(0,0) to exit position(3,3) and can move up, down, left, and right only
-
-Recursive Problem :
-The maze -solving process natually breaks into smaler subproblems.
-Each step attempts to solve a smaller verstion of the maze.
-
-< Analysis >
-Time Complexity : O(4^(N^2)) -> Each cell can branch into 4 direstion 
-Space Complexity : O(N^2) -> Visited matrix, recursion stack depth 
-
-< Test Case 1 >
-Start (0,0)
-End (3,3)
-Find path from (0,0) to (3,3)
-
-Iuput maze :
-0 1 0 0 
-0 0 0 1
-1 1 0 0 
-0 0 0 0 
-
-Output :
-[(0,0), (1,0), (1,1), (1,2), (2,2), (3,2), (3,3)]
-
-Edge Cases:
-start = end
-completely bolcked maze
-no possible path
-1x1 maze
 """
 
 def is_path(maze, x, y, visited):
